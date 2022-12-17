279586ccfa6c0ac84794e4966cc5aa5023ddab9d
AlexandraVamos <vamos_alexandra@yahoo.com>
2022-12-17 16:10:36 +0200

    add angular amazon project

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'myclone-21503',
+    appId: '1:541400137768:web:34eb4a5017dce4fa1cf51d',
+    storageBucket: 'myclone-21503.appspot.com',
+    apiKey: 'AIzaSyAq9NpVUtZi-JF8GiVvvYJernBtFYDtkJM',
+    authDomain: 'myclone-21503.firebaseapp.com',
+    messagingSenderId: '541400137768',
+    measurementId: 'G-B11D3R2HSV',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
